240e291a084dbd29928e533c923bdc793a79c629
habertrogena <habertorogy12@gmail.com>
2023-01-10 12:42:03 +0300

    feat:added (add item) feature

--- App.js
+++ App.js
@@ -1,49 +1,59 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View ,Image ,FlatList} from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList, Alert } from 'react-native';
 import Header from './components/Header';
 import uuid from 'react-uuid';
-
-
-
+import AddItem from './components/AddItem';
 import ListItems from './components/ListItems'
 
 
-  const App = () =>{
-    const [items,setItems] = useState([
-      {id:uuid(), text:"milk"},
-      {id:uuid(), text:"Bread"},
-      {id:uuid(), text:"Sugar"},
-      {id:uuid(), text:"Coffee"},
-      {id:uuid(), text:"BlueBand"},
-      {id:uuid(), text:"eggs"}
-    ])
+const App = () => {
+  const [items, setItems] = useState([
+    { id: uuid(), text: "milk" },
+    { id: uuid(), text: "Bread" },
+    { id: uuid(), text: "Sugar" },
+    { id: uuid(), text: "Coffee" },
+    { id: uuid(), text: "BlueBand" },
+    { id: uuid(), text: "eggs" }
+  ])
 
-    const deleteItem =(id) =>{
-      setItems(prevItems =>{
-        return prevItems.filter(item=>item.id != id);
-      });
-    }
-
-
-    
-    return(
-      <View style={styles.container} >
-        <Header/>
-        
-        <FlatList data={items} renderItem={({item})=>(
-          <ListItems item={item}
-          deleteItem={deleteItem} />
-        )} />
-      </View>
-    )
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
   }
 
-  const styles = StyleSheet.create({
-    container:{
-      flex:1,
-      paddingTop: 60
+  const addItem = (text) => {
+    if (!text) {
+      Alert.alert('Error', 'please add an item', { text: 'ok' })
+    } else {
+      setItems(prevItems => {
+        return [{ id: uuid(), text }, ...prevItems];
+      })
     }
-  })
+
+
+  }
+
+
+
+  return (
+    <View style={styles.container} >
+      <Header />
+      <AddItem addItem={addItem} />
+      <FlatList data={items} renderItem={({ item }) => (
+        <ListItems item={item}
+          deleteItem={deleteItem} />
+      )} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60
+  }
+})
 
 export default App;
--- components/AddItem.js
+++ components/AddItem.js
@@ -1,45 +1,49 @@
-// import React from "react";
-// import { StyleSheet, Text, View ,Image ,TouchableOpacity,TextInput} from 'react-native';
-// import { useState } from 'react';
+import React from "react";
+import { StyleSheet, Text, View ,Image ,TouchableOpacity,TextInput} from 'react-native';
+import { useState } from 'react';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 
 
-// const AddItem = ({title,AddItem}) => {
+const AddItem = ({title,addItem}) => {
 
-//     const [text,setText]=useState('');
-//     const onchange = textvalue => setText(textvalue);
-//   return(
-//     <View>
-//         <TextInput placeholder="Add Item..." style={styles.input}  onChangeText={onchange} />
-//         <TouchableOpacity  style={styles.btn} onPress={()=>{
-//             AddItem(text);
-//         }} >
-//             <Text style={styles.btnText} > Add Item</Text>
-
-//         </TouchableOpacity>
-//     </View>
-//   )
-// };
+    const [text,setText]=useState('');
+    const onchange = (textValue) => setText(textValue);
 
 
+  return(
+    <View>
+        <TextInput placeholder="Add Item..." style={styles.input}  onChangeText={onchange} />
 
-// const styles = StyleSheet.create({
-//  input:{
-//     height:60,
-//     padding: 8,
-//     fontSize: 16
-//  },
-//  btn:{
-//     padding: 9,
-//     margin:5,
-//     backgroundColor:'#c2bad8'
-//  },
-//  btnText:{
-//     color:'blue',
-//     fontSize: 20,
-//     textAlign:'center'
-//  }
-// });
+        <TouchableOpacity  style={styles.btn} onPress={()=>{
+            addItem(text);
+        }} >
+            <Text style={styles.btnText} > Add Item </Text>
+
+        </TouchableOpacity>
+    </View>
+  )
+};
 
 
-// export default AddItem;
+
+const styles = StyleSheet.create({
+ input:{
+    height:60,
+    padding: 8,
+    fontSize: 16
+ },
+ btn:{
+    padding: 9,
+    margin:5,
+    backgroundColor:'#c2bad8'
+ },
+ btnText:{
+    color:'blue',
+    fontSize: 20,
+    textAlign:'center'
+ }
+});
+
+
+export default AddItem;
